f76cf2aecc1f0b758556f2f309f22b4e0f8fd180
jorge-dev <jo.luisa94@gmail.com>
2024-11-01 11:03:17 -0600

    chore(): Remove unused initial migration SQL files and associated extensions

--- migrations/000000_init.down.sql
+++
@@ -1,7 +0,0 @@
--- Drop extensions in reverse order
-DROP EXTENSION IF EXISTS "citext";
-DROP EXTENSION IF EXISTS "pgcrypto";
-DROP EXTENSION IF EXISTS "uuid-ossp";
-
--- Reset timezone to default
-SET timezone = 'GMT';
--- migrations/000000_init.up.sql
+++
@@ -1,7 +0,0 @@
--- Initialize common PostgreSQL extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";      -- For UUID generation
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";       -- For cryptographic functions
-CREATE EXTENSION IF NOT EXISTS "citext";         -- For case-insensitive text fields
-
--- Set timezone to UTC
-SET timezone = 'UTC';
