05da5fd6d2b2abcc963922760763319b1bc698f1
jorge-dev <jo.luisa94@gmail.com>
2024-11-01 23:22:12 -0600

    refactor(): Refactor route registration to include authentication and private routes

--- server/routes.go
+++ server/routes.go
@@ -7,15 +7,39 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+	"github.com/jorge-dev/centsible/internal/auth"
+	"github.com/jorge-dev/centsible/internal/repository"
+	"github.com/jorge-dev/centsible/server/handlers"
+	customMiddleware "github.com/jorge-dev/centsible/server/middleware"
 )
 
-func (s *Server) RegisterRoutes() http.Handler {
+func (s *Server) RegisterRoutes(conn *pgx.Conn, jwtManager auth.JWTManager) http.Handler {
+	queries := repository.New(conn)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	r.Get("/", s.HelloWorldHandler)
+	// Public routes
+	r.Group(func(r chi.Router) {
+		r.Get("/", s.HelloWorldHandler)
+		r.Get("/health", s.healthHandler)
 
-	r.Get("/health", s.healthHandler)
+	})
+
+	// Auth routes
+	r.Group(func(r chi.Router) {
+		authHandler := handlers.NewAuthHandler(queries, &jwtManager)
+		r.Post("/register", authHandler.Register)
+		r.Post("/login", authHandler.Login)
+	})
+
+	// Private routes
+	r.Group(func(r chi.Router) {
+		authMiddleware := customMiddleware.NewAuthMiddleware(&jwtManager)
+		r.Use(authMiddleware.AuthRequired)
+		// Add private
+	})
 
 	return r
 }
