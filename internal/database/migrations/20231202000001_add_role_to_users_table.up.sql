558c3836caee725669d576650721406daf44448e
jorge-dev <jo.luisa94@gmail.com>
2024-11-02 11:40:55 -0600

    feat(): Update default value for role column in users table to 'User'

--- internal/database/migrations/20231202000001_add_role_to_users_table.up.sql
+++ internal/database/migrations/20231202000001_add_role_to_users_table.up.sql
@@ -1,2 +1,2 @@
 ALTER TABLE users
-ADD COLUMN role VARCHAR(50) NOT NULL DEFAULT 'user' CHECK (role IN ('Admin', 'User', 'Guest', 'Moderator', 'Editor', 'Viewer', 'Manager'));
+ADD COLUMN role VARCHAR(50) NOT NULL DEFAULT 'User' CHECK (role IN ('Admin', 'User', 'Guest', 'Moderator', 'Editor', 'Viewer', 'Manager'));
