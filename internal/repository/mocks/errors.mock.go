54306984089cbcf0b3d6f20086e655684100f696
jorge-dev <jo.luisa94@gmail.com>
2024-11-30 17:30:24 -0700

    feat(repository): Add email existence check and user creation logic in mocks

--- internal/repository/mocks/errors.mock.go
+++ internal/repository/mocks/errors.mock.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrRecordNotFound = errors.New("record not found")
 	ErrInvalidInput   = errors.New("invalid input")
+	ErrDuplicateKey   = errors.New("duplicate key value violates unique constraint")
 )
